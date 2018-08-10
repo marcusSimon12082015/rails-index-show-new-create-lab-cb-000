e4e71b7049fa44e1ccfa9d9089e50363a496b9d1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-10 10:18:16 +0000

    automatically backed up by learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,14 @@
+class CouponsController < ApplicationController 
+  def index 
+  end 
+
+  def create 
+  end 
+
+  def new 
+  end 
+
+  def show 
+    @coupon = Coupon.find(params[:id])
+  end 
+end 
