c060162a11eb7f2ecb83333bff887a9ee7f1d05f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-08-10 10:41:00 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,11 +4,11 @@ class CouponsController < ApplicationController
   end
 
   def create
-    byebug
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
+    byebug
     redirect_to coupon_path(@coupon)
   end
 
